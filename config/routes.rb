a117ea7064c57a1fc0ef599b725e606e6a538505
An.nA <anna.git3@gmail.com>
2015-01-07 00:22:18 -0800

    add a static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/home'
+
   get "articles/" => "articles#index"
   get 'articles/new' => 'articles#new', as: :new_article
   get 'articles/:id' => 'articles#show', as: :article
